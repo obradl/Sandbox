11a7f0872f6e6d60a310326d02a3fe2d1f867073
Øystein Brådland <oystein.bradland@bouvet.no>
2018-09-25 10:05:20 +0200

    Githubservice. Public -> private

--- src/Blog.Infrastructure/ApiClients/GitHubService.cs
+++ src/Blog.Infrastructure/ApiClients/GitHubService.cs
@@ -7,20 +7,18 @@ namespace Blog.Infrastructure.ApiClients
 {
     public class GitHubService
     {
-        public HttpClient Client { get; }
+        private readonly HttpClient _client;
 
         public GitHubService(HttpClient client)
         {
             client.BaseAddress = new Uri("https://github.com/");
-            Client = client;
+            _client = client;
         }
 
         public async Task<HttpStatusCode> GetFrontPage()
         {
-            var response = await Client.GetAsync("/");
-
+            var response = await _client.GetAsync("/");
             return response.StatusCode;
-
         }
     }
 }
